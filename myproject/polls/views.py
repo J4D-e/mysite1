28f745a9192994cf13f72b0eb8f47120ed0a217a
J4D-e <129479058+J4D-e@users.noreply.github.com>
2024-05-18 12:22:57 -0600

    Update views.py

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+import requests
+def rickymorty (request):
+   # URL de la API
+   api
+   _url = 'https://rickandmortyapi.com/api/character/2'
+   # Realizar solicitud a la API
+   response = requests.get (api_url)
+   data = []
+   if response. status_code == 200:
+      data = response. json ()
+      print (data)
+  return render(request, 'polls/testapirick.html', {'data': data})
